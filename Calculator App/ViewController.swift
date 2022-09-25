6118a0a4063c1e0a31ac322fb07711c5fbc36a1b
Bing <bpan2@users.noreply.github.com>
2022-09-25 15:09:11 -0400

    first milestone

---
+++ Calculator App/ViewController.swift
@@ -0,0 +1,31 @@
+/*
+ * File Name: Calculator Milestone One
+ * Last Modified: 25/09/2022
+ * Version1
+ *
+ * Author: Bing Pan
+ * Student ID: 301317241
+ *
+ * Author: Wilson Mungai
+ * Student ID: 301287641
+ *
+ * Author: Gideon Shewana
+ * Student ID: 301195054
+ *
+ * Description: It allows users to add, subtract, multiply, divide integers as well as decimal numbers and give results. During the input it allows users to change or clear what they already typed by pressing the back button. It also allows users to delete all the inputs by AC button and also allows users to remove the latest entry by pressing CE button
+ */
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
